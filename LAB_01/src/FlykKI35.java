a9a121eac66f941a4e659605e7413ff0c0bb7ee8
vzhik <menflik3@gmail.com>
2022-08-29 16:58:44 +0300

    LAB_01

---
+++ LAB_01/src/FlykKI35.java
@@ -0,0 +1,9 @@
+public class FlykKI35 {
+    public static void main(String[] args)
+    {
+        String _student = new String();
+        _student = "Flyk Yevhenii,KI-35";
+
+        System.out.println(_student);
+    }
+}
